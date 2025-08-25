8d3ac9211377b580e0a883dc1cebe65a5542afcc
Miku2077 <1652231043@qq.com>
2025-08-25 21:41:05 +0800

    感谢增强版适配作者：melvinvdb

--- EnhancedNativeTrainer/src/features/script.cpp
+++ EnhancedNativeTrainer/src/features/script.cpp
@@ -2314,12 +2314,12 @@ void process_localization_menu() {
 		{ versionDisplay.c_str(), NULL, NULL, true},    // 动态显示版本号
 		{ "原作者：Flying-Scotsmar,  Slash_Alex", NULL, NULL, true},
 		{ "基于 Alexander Blade 的 ScripthookV 构建", NULL, NULL, true},
+		{ "增强版适配作者：melvinvdb", NULL, NULL, true},
 		{ "感谢所有原开发者们，感谢帮助我的朋友们", NULL, NULL, true},
 		{ "汉化：随梦&而飞", NULL, NULL, true},
 		{ "感谢：烈火神君,  羽一大魔王", NULL, NULL, true},
 		{ "此版本和原英文版有较大区别", NULL, NULL, true},
 		{ "某些配置文件并不互相通用", NULL, NULL, true},
-		{ "使用此汉化版，必须删除以前的 DB 文件", NULL, NULL, true},
 		{ "标题添加 CN 就是为了区分其他版本", NULL, NULL, true},
 	};
 
