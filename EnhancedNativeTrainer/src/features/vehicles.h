f0cbb56ef40256c5a008ade663281f407ab77e38
Miku2077 <1652231043@qq.com>
2026-03-02 23:32:31 +0800

    火眼金睛，辨真车实名！
    POLDOM10是假的车辆模型名，只是表面显示的模型名，不能用于生成车辆。
    POLDOM10的真模型名为：POLDOMINATOR10，预览图为 poldom10（candc_dlc_2024_1/poldom10）

--- EnhancedNativeTrainer/src/features/vehicles.h
+++ EnhancedNativeTrainer/src/features/vehicles.h
@@ -1075,7 +1075,7 @@ const std::vector<VehicleImage> INGAME_VEH_IMAGES =
 	{ RAGE_JOAAT("PIZZABOY"), "sssa_dlc_2024_1", "pizzaboy" },
 	{ RAGE_JOAAT("VORSCHLAG"), "sssa_dlc_2024_1", "vorschlag" },
 	{ RAGE_JOAAT("YOSEMITE1500"), "ENT_vehicle_previews", "VP_YOSEMITE4" },
-	{ RAGE_JOAAT("POLDOM10"), "candc_dlc_2024_1", "poldom10" },
+	//{ RAGE_JOAAT("POLDOM10"), "candc_dlc_2024_1", "poldom10" },// POLDOM10 模型名称错误！真实的模型名称为 POLDOMINATOR10
 	{ RAGE_JOAAT("POLDORADO"), "candc_dlc_2024_1", "poldorado" },
 	{ RAGE_JOAAT("POLGREENWOOD"), "ENT_vehicle_previews", "VP_POLGREENW" },
 	{ RAGE_JOAAT("POLIMPALER5"), "candc_dlc_2024_1", "polimpaler5" },
@@ -1086,7 +1086,7 @@ const std::vector<VehicleImage> INGAME_VEH_IMAGES =
 	{ RAGE_JOAAT("DRIFTCYPHER"), "ENT_vehicle_previews", "VP_CYPHER" }, //
 	{ RAGE_JOAAT("DRIFTSENTINEL"), "ENT_vehicle_previews", "VP_SENTINELWIDE" }, //
 	{ RAGE_JOAAT("POLICET3"), "ENT_vehicle_previews", "VP_BURRITOBAIL" },
-	//{ RAGE_JOAAT("POLDOMINATOR10"), "ENT_vehicle_previews", "VP_DOMINATORFX" }, //
+	{ RAGE_JOAAT("POLDOMINATOR10"), "candc_dlc_2024_1", "poldom10" }, // POLDOMINATOR10 真实的预览图为 poldom10
 	// 2024 圣诞节 - 联邦调查局 DLC
 	{ RAGE_JOAAT("CHAVOSV6"), "sssa_dlc_2024_2", "chavosv6" },
 	{ RAGE_JOAAT("DRIFTCHAVOSV6"), "sssa_dlc_2024_2", "chavosv6" },
