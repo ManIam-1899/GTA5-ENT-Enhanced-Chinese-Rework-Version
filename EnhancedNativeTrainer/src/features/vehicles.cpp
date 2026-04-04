d83b398c9a3a4a242b48a4f4408abf3e1b87ff2a
Miku2077 <1652231043@qq.com>
2026-04-04 11:55:58 +0800

    生成司机代驾选项，新增推开其他载具功能。
    - 新增推开其他载具功能，可在司机代驾菜单中开启。
    - 开启时会自动禁用载具能量护盾功能，避免冲突。
    - 推开其他载具功能，通过检测前方载具并施加物理力推开。
    - 可配置推开范围（5-20米），并排除飞行器和水上水下载具。

--- EnhancedNativeTrainer/src/features/vehicles.cpp
+++ EnhancedNativeTrainer/src/features/vehicles.cpp
@@ -22,6 +22,7 @@ https://github.com/gtav-ent/GTAV-EnhancedNativeTrainer
 #include "..\io\config_io.h"
 #include "..\debug\debuglog.h"
 #include "area_effect.h"
+#include "drive_to_marker.h"
 #include <fstream>
 #include <sstream>
 #include "vehicle_weapons.h"
@@ -7861,6 +7862,7 @@ void onchange_veh_never_dirty(int value, SelectFromListMenuItem* source) {
 
 void onchange_veh_mass_index(int value, SelectFromListMenuItem* source){
 	VehMassMultIndex = value;
+	if (VehMassMultIndex > 0) featurePushVehiclesAway = false;
 	PositionChanged = true;
 }
 
